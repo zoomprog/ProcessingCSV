d592ff5a745de54283027c8b118005832dd2cf5f
rrarr <rrarrk123@gmail.com>
2025-06-19 13:42:41 +0300

    Добавил фильтрацию.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import csv
-import sys
 from tabulate import tabulate
 
 
@@ -9,6 +8,56 @@ def output_table_console(rows):
         headers = list(rows[0].keys())
         table_date = [[row[headers] for headers in headers] for row in rows]
         print(tabulate(table_date, headers=headers, tablefmt="grid"))
+
+def filter_table(rows, condition_filter):
+    if not condition_filter:
+        return rows
+
+    condition = ' '.join(condition_filter.split())
+
+    for op in ['=', '>', '<']:
+        if op in condition:
+            left, right = condition.split(op, 1)
+            break
+    else:
+        return rows
+
+    filtered = []
+    for row in rows:
+        try:
+            row_value = row[left]
+
+            is_numeric = True
+            try:
+                row_val_num = float(row_value)
+                right_val_num = float(right.strip("'\""))
+            except ValueError:
+                is_numeric = False
+                row_val_num = str(row_value).strip().lower()
+                right_val_num = right.strip("'\"").lower()
+
+
+            if is_numeric:
+                match = {
+                    '>': lambda a, b: a > b,
+                    '<': lambda a, b: a < b,
+                    '=': lambda a, b: a == b,
+                }[op](row_val_num, right_val_num)
+            else:
+                if op == '==':
+                    match = row_val_num == right_val_num
+                elif op == '=':
+                    match = right_val_num in row_val_num
+                else:
+                    match = False
+
+            if match:
+                filtered.append(row)
+
+        except KeyError:
+            continue
+
+    return filtered
 
 
 def main():
@@ -20,7 +69,9 @@ def main():
     with open(args.file, 'r') as file:
         csv_reader = csv.DictReader(file)
         rows = list(csv_reader)
-    output_table_console(rows)
+    filtered_rows = filter_table(rows, args.where)
+    output_table_console(filtered_rows)
+
 
 if __name__ == '__main__':
     main()
